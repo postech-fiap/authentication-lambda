a913e8e127470d9639d9d250b51d15f03b548c4d
Gabriel Garcia <gabrielgarciasp@gmail.com>
2023-10-16 00:21:13 -0300

    First version

---
+++ index.js
@@ -0,0 +1,43 @@
+const jwt = require('jsonwebtoken')
+
+const key = 'private-key'
+
+const generateRandomToken = () => {
+    const payload = {
+        anonymous: true
+    }
+    return jwt.sign(payload, key)
+}
+
+const generateToken = (cpf) => {
+    const payload = { cpf }
+    return jwt.sign(payload, key)
+}
+
+const makeSuccessResponse = (token) => {
+    return {
+        statusCode: 200,
+        body: {
+            token
+        }
+      }
+}
+
+const makeErrorResponse = () => {
+    return {
+        statusCode: 500
+      }
+}
+
+exports.handler = async function(event, context) {
+    const cpf = event.cpf
+
+    if (cpf == null) {
+        const randomToken = generateRandomToken()
+        return makeSuccessResponse(randomToken)
+    }
+
+    const token = generateToken(cpf)
+
+    return makeSuccessResponse(token)
+}
