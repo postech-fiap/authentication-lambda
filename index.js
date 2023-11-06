624a4f6736eba1981fd3e80da9d2fa6139b56087
Gabriel Garcia <gabrielgarciasp@gmail.com>
2023-11-06 17:12:04 -0300

    Final version

--- index.js
+++ index.js
@@ -1,43 +1,36 @@
 const jwt = require('jsonwebtoken')
+const mysql = require('mysql2/promise');
 
-const key = 'private-key'
+const jwtSecret = process.env.JWT_SECRET
 
-const generateRandomToken = () => {
-    const payload = {
-        anonymous: true
-    }
-    return jwt.sign(payload, key)
-}
-
-const generateToken = (cpf) => {
-    const payload = { cpf }
-    return jwt.sign(payload, key)
-}
-
-const makeSuccessResponse = (token) => {
-    return {
-        statusCode: 200,
-        body: {
-            token
-        }
-      }
-}
-
-const makeErrorResponse = () => {
-    return {
-        statusCode: 500
-      }
+const generateToken = (id) => {
+    const payload = { id }
+    const options = { expiresIn: '20m' }
+    return jwt.sign(payload, jwtSecret, options)
 }
 
 exports.handler = async function(event, context) {
     const cpf = event?.cpf
 
-    if (cpf == null) {
-        const randomToken = generateRandomToken()
-        return makeSuccessResponse(randomToken)
+    if (!cpf) {
+        return generateToken(null)
     }
 
-    const token = generateToken(cpf)
+    const connection = await mysql.createConnection({
+        host: process.env.RDS_HOSTNAME,
+        port: process.env.RDS_PORT,
+        user: process.env.RDS_USERNAME,
+        password: process.env.RDS_PASSWORD,
+        database: process.env.RDS_DATABASE
+    })
 
-    return makeSuccessResponse(token)
+    const [rows] = await connection.execute('SELECT * FROM `cliente` WHERE `cpf` = ?', [cpf]);
+
+    if (rows.length == 0) {
+        throw 'User not found' 
+    }
+
+    const id = rows[0].id
+
+    return generateToken(id)
 }
